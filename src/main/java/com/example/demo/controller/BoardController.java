0bbab1e659426fc582d5ea7c6e070be73088a7fc
dkakwhs12396@naver.com <hp_TIiMkXScdu02vAPZzrtEyuiNL511Bv2FrZKk>
2022-01-13 06:05:13 +0900

    페이징처리중

--- src/main/java/com/example/demo/controller/BoardController.java
+++ src/main/java/com/example/demo/controller/BoardController.java
@@ -24,9 +24,25 @@ public class BoardController {
     @GetMapping("/boardList.do")
     public String boardList(Model model)
     {
-        Page<BoardEntity> list = boardService.getBoardList(0,10);
-        System.out.println("리스트 가져오기 성공");
+
+        Page<BoardEntity> list = boardService.getBoardList(4,10);
+
+        int nowPage = 3;//기본현재페이지
+        //블럭계산
+        int pagePerBlock = 15; //한페이지당 표시할블럭수
+        Long totalRecord = list.getTotalElements(); //전체 게시물(레코드)수
+        int totalPage =list.getTotalPages(); //전체 페이지수 size 10으로해서 10개의 개시물로 나눴을때 나오는 페이지수
+        int nowBlock = (int)Math.ceil((double) nowPage/pagePerBlock); //현재 블럭번호
+        int totalBlock = (int)Math.ceil((double)totalPage/pagePerBlock); //전체 블럭수
+
+        //블럭에 표시할 StartNum,EndNum 계산(한페이지당 표시할블럭수(15개)에 1부터15까지 계산)
+        //만약 2블럭이면 1블럭은 1-15페이지고 2블럭은 16페이지부터 31페이지까지를 계산해야댐
+        int pageStart =(nowPage-1)*pagePerBlock+1;//1블럭은1 2블럭은 16 3블럭은 32
+        int pageEnd = 1;//진행중
+
+
         model.addAttribute("list",list);
+
         return "/view/board/boardList.html";
     }
 
